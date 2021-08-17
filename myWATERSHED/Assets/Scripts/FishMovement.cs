86b82c9b731107bb182e4e3535923082f893fbf9
Amelia-McLeavey <mcleavea@sheridancollege.ca>
2021-08-17 16:08:44 -0400

    Implement basic fish movement for fish cam scene
    Uses transform and lerp

---
+++ myWATERSHED/Assets/Scripts/FishMovement.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FishMovement : MonoBehaviour
+{
+    [SerializeField]
+    private float agility = 1.0f;
+
+    private Vector3 nextPosition;
+
+    private void Start()
+    {
+        nextPosition = new Vector3(Random.Range(-0.5f, 3.5f), Random.Range(0.5f, 2.5f), Random.Range(-7f, -4f));
+    }
+
+    private void Update()
+    {
+        Vector3 heading = nextPosition - transform.position;
+        float distance = heading.magnitude;
+
+        if (distance <= 0.5f)
+        {
+            nextPosition = new Vector3(Random.Range(-0.5f, 3.5f), Random.Range(0.5f, 2.5f), Random.Range(-7f, -4f));
+        }
+
+        transform.position = Vector3.Lerp(transform.position, nextPosition, agility * Time.deltaTime);
+    }
+}
