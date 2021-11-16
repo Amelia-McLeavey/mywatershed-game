0e627a7419b187bda7d3f75207ae345fe20c550e
adkehn <kehn@sheridancollege.ca>
2021-11-16 16:57:54 -0500

    Variable data from tiles passes to UI display

--- myWATERSHED/Assets/Scripts/Controllers/PrototypeController.cs
+++ myWATERSHED/Assets/Scripts/Controllers/PrototypeController.cs
@@ -67,13 +67,21 @@ public class PrototypeController : MonoBehaviour
         // TILE CLICKS
 
         //if (activeTileUI == true) {
+        //prob dont need the bool can just check if the game object is active
         //    //display and update variables based on tile raycasted
+
+        /*
+        pseudo:
+       - store the tile in a gameobject holder so we can keep referrencing the variables updating outside of the
+       raycast
+       - 
+        */
 
         //}
         if (Input.GetMouseButtonDown(1)) // left mouse click
         {
             //toggle tile ui on right click
-            if (activeTileUI == false) { activeTileUI = true; } else if (activeTileUI == true) { activeTileUI = false; }
+            if (activeTileUI == false) { activeTileUI = true; tileUI.SetActive(true); } else if (activeTileUI == true) { activeTileUI = false; tileUI.SetActive(false); }
 
 
             // Create a ray from the point clicked on screen to the point in world space
@@ -82,9 +90,14 @@ public class PrototypeController : MonoBehaviour
             // Pass ray into Raycast to get hit info
             if (Physics.Raycast(ray, out RaycastHit hit))
             {
-                Debug.Log(hit.collider.gameObject.tag);
+                //Debug.Log(hit.collider.gameObject.tag);
+                
+                //pass tile type to UI
                 tileType.text = hit.collider.gameObject.tag;
-                if(hit.collider.gameObject.tag == "Agriculture") {
+               
+                //all the land types
+                if (hit.collider.gameObject.tag == "Agriculture" || hit.collider.gameObject.tag == "Commercial" || hit.collider.gameObject.tag == "EstateResidential" || hit.collider.gameObject.tag == "Forest" || hit.collider.gameObject.tag == "GolfCourse" || hit.collider.gameObject.tag == "HighDensity" || hit.collider.gameObject.tag == "Highway" || hit.collider.gameObject.tag == "Industrial" || hit.collider.gameObject.tag == "Institutional" || hit.collider.gameObject.tag == "LowMidDensity" || hit.collider.gameObject.tag == "Meadow" || hit.collider.gameObject.tag == "RecreationCentreSpace" || hit.collider.gameObject.tag == "Successional" || hit.collider.gameObject.tag == "UrbanOpenSpace" || hit.collider.gameObject.tag == "Vacant")
+                {
                     variable1.text = "Asphalt Density";
                     variable1data.text = hit.collider.gameObject.GetComponent<AsphaltDensity>().m_AsphaltDensity.ToString();
 
@@ -98,22 +111,37 @@ public class PrototypeController : MonoBehaviour
                     variable4data.text = hit.collider.gameObject.GetComponent<WaterTemperature>().m_waterTemperature.ToString();
 
                     variable5.text = "Pollution Level";
-                    variable5data.text =  hit.collider.gameObject.GetComponent<PollutionLevel>().m_PolutionLevel.ToString();
+                    variable5data.text = hit.collider.gameObject.GetComponent<PollutionLevel>().m_PolutionLevel.ToString();
 
                     variable6.text = "Sewage Level";
                     variable6data.text = hit.collider.gameObject.GetComponent<SewageLevel>().m_SewageLevel.ToString();
                 }
-                
-                
-                /*
-             pseudo:
-             - get the tag from the tile
-             - based on what we know from the components assigned to tags, get the relevant variable info and pass it to the UI element
-             - activate the ui game object so we can view it
-             - maybe...start a coroutine to keep updating the ui element so we can see the changes while it is active? or...need a way to keep it updating based on current tile.
-             - click again and ui element is closed, can just use a bool like "activeTileUI" or something.
+
+                //all the water types
+                if (hit.collider.gameObject.tag == "EngineeredReservoir" || hit.collider.gameObject.tag == "EngineeredStream" || hit.collider.gameObject.tag == "NaturalStream" || hit.collider.gameObject.tag == "Wetland")
+                {
+                    variable1.text = "Brown Trout Population";
+                    variable1data.text = hit.collider.gameObject.GetComponent<BrownTroutPopulation>().m_BrownTroutPopulation.ToString();
+
+                    variable2.text = "Creek Chub Population";
+                    variable2data.text = hit.collider.gameObject.GetComponent<CreekChubPopulation>().m_CreekChubPopulation.ToString();
+
+                    variable3.text = "Insect Population";
+                    variable3data.text = hit.collider.gameObject.GetComponent<InsectPopulation>().m_InsectPopulation.ToString();
+
+                    variable4.text = "Rate of Flow";
+                    variable4data.text = hit.collider.gameObject.GetComponent<RateOfFlow>().m_RateOfFlow.ToString();
+
+                    variable5.text = "Red Dace Population";
+                    variable5data.text = hit.collider.gameObject.GetComponent<RedDacePopulation>().m_RedDacePopulation.ToString();
+
+                    variable6.text = "Sewage Level";
+                    variable6data.text = hit.collider.gameObject.GetComponent<SewageLevel>().m_SewageLevel.ToString();
+                }
+
              
-             */
+           
+
             }
         }
 
