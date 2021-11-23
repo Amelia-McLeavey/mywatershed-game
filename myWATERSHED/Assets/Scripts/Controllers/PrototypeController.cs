dfc13b0ce5235279befb2ba621023af6778a4db3
adkehn <kehn@sheridancollege.ca>
2021-11-23 18:41:56 -0500

    Updated Prototype controller to display changing variables better

--- myWATERSHED/Assets/Scripts/Controllers/PrototypeController.cs
+++ myWATERSHED/Assets/Scripts/Controllers/PrototypeController.cs
@@ -37,6 +37,8 @@ public class PrototypeController : MonoBehaviour
     [SerializeField] Text variable5data;
     [SerializeField] Text variable6data;
 
+    public GameObject variableHolder;
+
 
     public void LoadScene(string sceneName)
     {
@@ -60,29 +62,74 @@ public class PrototypeController : MonoBehaviour
         m_worldGenScript.m_Seed--;
     }
 
+    private void Start()
+    {
+        variableHolder = null;
+    }
+
     private void Update()
     {
 
 
-        // TILE CLICKS
+        //all the land types
+        if (variableHolder != null)
+        {
+            tileType.text = variableHolder.tag;
 
-        //if (activeTileUI == true) {
-        //prob dont need the bool can just check if the game object is active
-        //    //display and update variables based on tile raycasted
+            if (variableHolder.tag == "Agriculture" || variableHolder.tag == "Commercial" || variableHolder.tag == "EstateResidential" || variableHolder.tag == "Forest" || variableHolder.tag == "GolfCourse" || variableHolder.tag == "HighDensity" || variableHolder.tag == "Highway" || variableHolder.tag == "Industrial" || variableHolder.tag == "Institutional" || variableHolder.tag == "LowMidDensity" || variableHolder.tag == "Meadow" || variableHolder.tag == "RecreationCentreSpace" || variableHolder.tag == "Successional" || variableHolder.tag == "UrbanOpenSpace" || variableHolder.tag == "Vacant")
+            {
+                variable1.text = "Asphalt Density";
+                variable1data.text = variableHolder.GetComponent<AsphaltDensity>().m_AsphaltDensity.ToString();
 
-        /*
-        pseudo:
-       - store the tile in a gameobject holder so we can keep referrencing the variables updating outside of the
-       raycast
-       - 
-        */
+                variable2.text = "Erosion Rate";
+                variable2data.text = variableHolder.GetComponent<ErosionRate>().m_ErosionRate.ToString();
+
+                variable3.text = "Land Height";
+                variable3data.text = variableHolder.GetComponent<LandHeight>().m_LandHeight.ToString();
+
+                variable4.text = "Water Temperature";
+                variable4data.text = variableHolder.GetComponent<WaterTemperature>().m_waterTemperature.ToString();
+
+                variable5.text = "Pollution Level";
+                variable5data.text = variableHolder.GetComponent<PollutionLevel>().m_PolutionLevel.ToString();
+
+                variable6.text = "Sewage Level";
+                variable6data.text = variableHolder.GetComponent<SewageLevel>().m_SewageLevel.ToString();
+            }
+
+            //all the water types
+            if (variableHolder.tag == "EngineeredReservoir" || variableHolder.tag == "EngineeredStream" || variableHolder.tag == "NaturalStream" || variableHolder.tag == "Wetland")
+            {
+                variable1.text = "Brown Trout Population";
+                variable1data.text = variableHolder.GetComponent<BrownTroutPopulation>().m_BrownTroutPopulation.ToString();
+
+                variable2.text = "Creek Chub Population";
+                variable2data.text = variableHolder.GetComponent<CreekChubPopulation>().m_CreekChubPopulation.ToString();
+
+                variable3.text = "Insect Population";
+                variable3data.text = variableHolder.GetComponent<InsectPopulation>().m_InsectPopulation.ToString();
+
+                variable4.text = "Rate of Flow";
+                variable4data.text = variableHolder.GetComponent<RateOfFlow>().m_RateOfFlow.ToString();
+
+                variable5.text = "Red Dace Population";
+                variable5data.text = variableHolder.GetComponent<RedDacePopulation>().m_RedDacePopulation.ToString();
+
+                variable6.text = "Sewage Level";
+                variable6data.text = variableHolder.GetComponent<SewageLevel>().m_SewageLevel.ToString();
+            }
+        }
+
 
         //}
-        if (Input.GetMouseButtonDown(1)) // left mouse click
+        if (Input.GetMouseButtonDown(1)) // right mouse click
         {
-            //toggle tile ui on right click
-            if (activeTileUI == false) { activeTileUI = true; tileUI.SetActive(true); } else if (activeTileUI == true) { activeTileUI = false; tileUI.SetActive(false); }
+            //temporarily removed the clicking to turn on/off, it's always visible for simplicity in this prototype. can easily change later
 
+            //toggle tile ui on right click
+            //if (activeTileUI == false) { activeTileUI = true; tileUI.SetActive(true); } else if (activeTileUI == true) { activeTileUI = false; tileUI.SetActive(false); }
+
+            //ADD SOME VISUAL FEEDBACK TO SHOW WHICH TILE IS CURRENTLY SELECTED
 
             // Create a ray from the point clicked on screen to the point in world space
             Ray ray = m_camera.ScreenPointToRay(Input.mousePosition);
@@ -90,57 +137,59 @@ public class PrototypeController : MonoBehaviour
             // Pass ray into Raycast to get hit info
             if (Physics.Raycast(ray, out RaycastHit hit))
             {
+                variableHolder = hit.collider.gameObject;
+
                 //Debug.Log(hit.collider.gameObject.tag);
-                
+
                 //pass tile type to UI
-                tileType.text = hit.collider.gameObject.tag;
-               
-                //all the land types
-                if (hit.collider.gameObject.tag == "Agriculture" || hit.collider.gameObject.tag == "Commercial" || hit.collider.gameObject.tag == "EstateResidential" || hit.collider.gameObject.tag == "Forest" || hit.collider.gameObject.tag == "GolfCourse" || hit.collider.gameObject.tag == "HighDensity" || hit.collider.gameObject.tag == "Highway" || hit.collider.gameObject.tag == "Industrial" || hit.collider.gameObject.tag == "Institutional" || hit.collider.gameObject.tag == "LowMidDensity" || hit.collider.gameObject.tag == "Meadow" || hit.collider.gameObject.tag == "RecreationCentreSpace" || hit.collider.gameObject.tag == "Successional" || hit.collider.gameObject.tag == "UrbanOpenSpace" || hit.collider.gameObject.tag == "Vacant")
-                {
-                    variable1.text = "Asphalt Density";
-                    variable1data.text = hit.collider.gameObject.GetComponent<AsphaltDensity>().m_AsphaltDensity.ToString();
+                //tileType.text = hit.collider.gameObject.tag;
 
-                    variable2.text = "Erosion Rate";
-                    variable2data.text = hit.collider.gameObject.GetComponent<ErosionRate>().m_ErosionRate.ToString();
+                ////all the land types
+                //if (hit.collider.gameObject.tag == "Agriculture" || hit.collider.gameObject.tag == "Commercial" || hit.collider.gameObject.tag == "EstateResidential" || hit.collider.gameObject.tag == "Forest" || hit.collider.gameObject.tag == "GolfCourse" || hit.collider.gameObject.tag == "HighDensity" || hit.collider.gameObject.tag == "Highway" || hit.collider.gameObject.tag == "Industrial" || hit.collider.gameObject.tag == "Institutional" || hit.collider.gameObject.tag == "LowMidDensity" || hit.collider.gameObject.tag == "Meadow" || hit.collider.gameObject.tag == "RecreationCentreSpace" || hit.collider.gameObject.tag == "Successional" || hit.collider.gameObject.tag == "UrbanOpenSpace" || hit.collider.gameObject.tag == "Vacant")
+                //{
+                //    variable1.text = "Asphalt Density";
+                //    variable1data.text = hit.collider.gameObject.GetComponent<AsphaltDensity>().m_AsphaltDensity.ToString();
 
-                    variable3.text = "Land Height";
-                    variable3data.text = hit.collider.gameObject.GetComponent<LandHeight>().m_LandHeight.ToString();
+                //    variable2.text = "Erosion Rate";
+                //    variable2data.text = hit.collider.gameObject.GetComponent<ErosionRate>().m_ErosionRate.ToString();
 
-                    variable4.text = "Water Temperature";
-                    variable4data.text = hit.collider.gameObject.GetComponent<WaterTemperature>().m_waterTemperature.ToString();
+                //    variable3.text = "Land Height";
+                //    variable3data.text = hit.collider.gameObject.GetComponent<LandHeight>().m_LandHeight.ToString();
 
-                    variable5.text = "Pollution Level";
-                    variable5data.text = hit.collider.gameObject.GetComponent<PollutionLevel>().m_PolutionLevel.ToString();
+                //    variable4.text = "Water Temperature";
+                //    variable4data.text = hit.collider.gameObject.GetComponent<WaterTemperature>().m_waterTemperature.ToString();
 
-                    variable6.text = "Sewage Level";
-                    variable6data.text = hit.collider.gameObject.GetComponent<SewageLevel>().m_SewageLevel.ToString();
-                }
+                //    variable5.text = "Pollution Level";
+                //    variable5data.text = hit.collider.gameObject.GetComponent<PollutionLevel>().m_PolutionLevel.ToString();
 
-                //all the water types
-                if (hit.collider.gameObject.tag == "EngineeredReservoir" || hit.collider.gameObject.tag == "EngineeredStream" || hit.collider.gameObject.tag == "NaturalStream" || hit.collider.gameObject.tag == "Wetland")
-                {
-                    variable1.text = "Brown Trout Population";
-                    variable1data.text = hit.collider.gameObject.GetComponent<BrownTroutPopulation>().m_BrownTroutPopulation.ToString();
+                //    variable6.text = "Sewage Level";
+                //    variable6data.text = hit.collider.gameObject.GetComponent<SewageLevel>().m_SewageLevel.ToString();
+                //}
 
-                    variable2.text = "Creek Chub Population";
-                    variable2data.text = hit.collider.gameObject.GetComponent<CreekChubPopulation>().m_CreekChubPopulation.ToString();
+                ////all the water types
+                //if (hit.collider.gameObject.tag == "EngineeredReservoir" || hit.collider.gameObject.tag == "EngineeredStream" || hit.collider.gameObject.tag == "NaturalStream" || hit.collider.gameObject.tag == "Wetland")
+                //{
+                //    variable1.text = "Brown Trout Population";
+                //    variable1data.text = hit.collider.gameObject.GetComponent<BrownTroutPopulation>().m_BrownTroutPopulation.ToString();
 
-                    variable3.text = "Insect Population";
-                    variable3data.text = hit.collider.gameObject.GetComponent<InsectPopulation>().m_InsectPopulation.ToString();
+                //    variable2.text = "Creek Chub Population";
+                //    variable2data.text = hit.collider.gameObject.GetComponent<CreekChubPopulation>().m_CreekChubPopulation.ToString();
 
-                    variable4.text = "Rate of Flow";
-                    variable4data.text = hit.collider.gameObject.GetComponent<RateOfFlow>().m_RateOfFlow.ToString();
+                //    variable3.text = "Insect Population";
+                //    variable3data.text = hit.collider.gameObject.GetComponent<InsectPopulation>().m_InsectPopulation.ToString();
 
-                    variable5.text = "Red Dace Population";
-                    variable5data.text = hit.collider.gameObject.GetComponent<RedDacePopulation>().m_RedDacePopulation.ToString();
+                //    variable4.text = "Rate of Flow";
+                //    variable4data.text = hit.collider.gameObject.GetComponent<RateOfFlow>().m_RateOfFlow.ToString();
 
-                    variable6.text = "Sewage Level";
-                    variable6data.text = hit.collider.gameObject.GetComponent<SewageLevel>().m_SewageLevel.ToString();
-                }
+                //    variable5.text = "Red Dace Population";
+                //    variable5data.text = hit.collider.gameObject.GetComponent<RedDacePopulation>().m_RedDacePopulation.ToString();
 
-             
-           
+                //    variable6.text = "Sewage Level";
+                //    variable6data.text = hit.collider.gameObject.GetComponent<SewageLevel>().m_SewageLevel.ToString();
+                //}
+
+
+
 
             }
         }
