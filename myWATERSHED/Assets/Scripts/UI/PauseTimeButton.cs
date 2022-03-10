f4668fd4709fb512aed342fdc9ef27d5fb57b086
adkehn <61705327+adkehn@users.noreply.github.com>
2022-03-10 14:33:23 -0800

    Tutorial Implemented

--- myWATERSHED/Assets/Scripts/UI/PauseTimeButton.cs
+++ myWATERSHED/Assets/Scripts/UI/PauseTimeButton.cs
@@ -2,24 +2,30 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
 
 public class PauseTimeButton : MonoBehaviour
 {
     private GameManager m_gameManager;
-
+    public Sprite[] sprite;
+     
     private void Start()
     {
         m_gameManager = GameManager.Instance;
+        
     }
     public void PauseClicked()
     {
         if(m_gameManager.m_gameState == GameState.Frozen)
         {
             m_gameManager.SetGameState(GameState.Game, null);
+            gameObject.GetComponent<Image>().sprite = sprite[0];
+            
         }
         else
         {
             m_gameManager.SetGameState(GameState.Frozen, null);
+            gameObject.GetComponent<Image>().sprite = sprite[1];
         }
     }
 
